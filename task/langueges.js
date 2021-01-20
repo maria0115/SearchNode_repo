9b8fe294e98ef281af047b69cfbb0b1f96fdee9c
maria0115 <rkddpdms12@naver.com>
2021-01-20 17:05:25 +0900

    langueges.js close

--- task/langueges.js
+++ task/langueges.js
@@ -3,6 +3,7 @@ const util = require("../lib/util.js");
 const logger = require("../lib/log.js");
 const axios = require("axios");
 const cookie = require('cookie');
+const fs = require('fs')
 
 function convert(config, qObj, res, es_res) {
     // console.log(es_res.data,"결과값");
@@ -23,21 +24,27 @@ function convert(config, qObj, res, es_res) {
     return ret;
 }
 
-var ret = {};
 async function getlangueges(config, qObj, res,req) {
     console.log('--------------')
     console.log(qObj);
     // var url = `${config.languages}?category=${qObj.locale}`;
     // console.log(url);
+    var ret = {};
     ret={};
 
     console.log(ret);
     // // console.log('cookie',req.headers.cookie);
-    const languages = require('../language.json');
-    const uselanguages = languages;
-    console.log(languages,"json파일");
-    languages.ko.searchresult = "검색어 <span class='highlight'>&quot;@&quot;</span>에 대한 <span class='highlight'>#</span>건의 검색 결과입니다.";
-    languages.en.searchresult = "Here are <span class='highlight'>#</span> results for the search term <span class='highlight'>&quot;@&quot;</span>.";
+    console.log(__dirname);
+    var contents = fs.readFileSync('d:/nodejs/searchdata/language.json', 'utf8');
+    fs.closeSync(2);
+    console.log(contents);
+    var uselanguages =JSON.parse(contents);
+    // const languages = require('../language.json');
+    // const uselanguages = languages;
+    console.log('------------------------------')
+    console.log(uselanguages,"uselanguagesjson파일");
+    // languages.ko.searchresult = "검색어 <span class='highlight'>&quot;@&quot;</span>에 대한 <span class='highlight'>#</span>건의 검색 결과입니다.";
+    // languages.en.searchresult = "Here are <span class='highlight'>#</span> results for the search term <span class='highlight'>&quot;@&quot;</span>.";
     // await axios({
     //     method: 'get',
     //     url: url,
@@ -59,13 +66,15 @@ async function getlangueges(config, qObj, res,req) {
     console.log(qObj.locale,"locale");
     var lang = '';
     if(qObj.locale===""){
-        ret = uselanguages[cookie.parse(qObj.cookie).language];
+        console.log(cookie.parse(qObj.cookie).language,"cookie 없음");
+        lang = cookie.parse(qObj.cookie).language;
+    
         // console.log(ret);
     }else{
-        
-        ret = uselanguages[qObj.locale];
+        lang = qObj.locale;
         // console.log(ret);
     }
+    ret = uselanguages[lang];
     console.log(qObj,"********************");
     console.log(ret);
 
@@ -76,8 +85,8 @@ async function getlangueges(config, qObj, res,req) {
     // res.end(JSON.stringify(ret));
     res.statusCode = 200;
     res.setHeader("Content-type", "application/json; charset=UTF-8");
-    ret.searchresult = ret.searchresult.replace(/@/,qObj.searchword);
-    ret.searchresult = ret.searchresult.replace(/#/,qObj.total_cnt);
+    ret.searchresult = uselanguages[lang].searchresult.replace(/@/,qObj.searchword);
+    ret.searchresult = uselanguages[lang]   .searchresult.replace(/#/,qObj.total_cnt);
     res.send(JSON.stringify(ret));
     return ret;
 };
