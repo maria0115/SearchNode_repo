eb0d2bc8c6976ab48e64e4c18724222d396e7c21
maria0115 <rkddpdms12@naver.com>
2021-01-20 16:14:54 +0900

    기간 수정

--- task/search/searchquery.js
+++ task/search/searchquery.js
@@ -8,7 +8,6 @@ async function SearchQuery(qObj) {
     var result = await Query(qObj);
     return result;
 }
-
 // _msearch
 async function MsearchQuery(qObj) {
     console.log("MsearchQuery 여기로 왔다")
@@ -22,45 +21,29 @@ async function MsearchQuery(qObj) {
     // "operator":"AND"}}},{"match":{"author.search":{"query":"김선호","operator":"AND"}}}]}},
     // "size":5,"from":0,"sort":{"created":{"order":"desc"}}}
     var q = await Query(qObj);
-
     var stringquery = '';
     var category = config.default_category;
     for (var i = 0; i < category.length; i++) {
         var query = {};
-
         var smallquery = {};
         var categ = category[i];
         smallquery.category = categ;
         var smallmatch = {};
         smallmatch.match = smallquery;
-        // console.log(q.query.bool)
         q.query.bool.filter[0] = smallmatch;
         query[i] = q;
 
         stringquery += JSON.stringify(index) + "\n";
         stringquery += JSON.stringify(query[i]) + "\n";
-        // console.log(stringquery, 'stringquery');
     }
-
     console.log(stringquery, "stringquery");
     return stringquery;
 }
-
 async function Query(qObj) {
     console.log(qObj);
     var esquery = {};
-    // console.log(qObj);
-    // if (qObj.searchwordarr.length > 0) {
-    //     // qObj.searchword = "";
-    //     // for (var i = 0; i < qObj.searchwordarr.length; i++) {
-    //     //     qObj.searchword += ` ${qObj.searchwordarr[i]}`;
-    //     // }
-    // }
-    // qObj.searchwordarr.push(qObj.searchword);
     console.log(qObj.searchwordarr);
     console.log(qObj.searchword);
-    // var searcharr = qObj.searchword.split(" ");
-    // console.log(searcharr);
     var query = {};
     if (qObj.searchword !== '') {
         var bool = {};
@@ -74,30 +57,10 @@ async function Query(qObj) {
         // console.log(q.query.bool)
         if (qObj.gte !== "default") {
             const utc = UtcDate(qObj.utc);
-            var lt = '';
-            var gte = "";
-            var format = "";
             var createdate = {};
-            if (qObj.dateType === "season" || qObj.dateType === "ago") {
-                gte = qObj.gte;
-                lt = "now";
-            } else if (qObj.dateType === "custom") {
-                gte = qObj.gte[0];
-                lt = qObj.gte[1];
-                format = "yyyyMMdd";
-                createdate.format = format;
-            }
-            // if (qObj.dateType === "season" || qObj.dateType === "ago") {
-            //     gte = qObj.gte;
-            //     lt = qObj.lt;
-            // } else if (qObj.dateType === "custom") {
-            //     gte = qObj.gte[0];
-            //     lt = qObj.gte[1];
-            // }
-            // format = "yyyyMMdd";
-            // createdate.format = format;
-            createdate.lt = lt;
-            createdate.gte = gte;
+            createdate.format = "yyyyMMddHHmmss";
+            createdate.lt = qObj.lt;
+            createdate.gte = qObj.gte;
             createdate['time_zone'] = utc;
             var range = {};
             range.created = createdate;
@@ -105,9 +68,7 @@ async function Query(qObj) {
             rangefilter.range = range;
             filter.push(rangefilter);
         }
-
         bool.filter = filter;
-
         var must = [];
         var fields = [];
         if (qObj.fieldname !== 'all') {
@@ -118,12 +79,7 @@ async function Query(qObj) {
                 fields.push(`${field}.search`);
             }
         }
-
         for (var i = 0; i < qObj.searchwordarr.length; i++) {
-            
-
-            
-
             if (qObj.searchwordarr[i].indexOf(" ") !== -1) {
                 var searcharr = qObj.searchwordarr[i].split(" ");
                 var should = [];
@@ -176,7 +132,6 @@ async function Query(qObj) {
     // console.log(JSON.stringify(esquery), "esquery");
     return esquery;
 }
-
 function UtcDate(utc) {
     var date = '';
     if (utc >= 0) {
